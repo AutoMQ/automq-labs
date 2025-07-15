df9fd64442efb62a9a927250ee7755ad1bc8c772
Gezi-lzq <lzqtxwd@gmail.com>
2025-07-15 16:43:52 +0800

    feat: add Terraform configuration for AutoMQ BYOC deployment on Amazo… (#43)
    * feat: add Terraform configuration for AutoMQ BYOC deployment on Amazon EKS
    
    * fix: correct indentation for EKS cluster security group output in Terraform
    
    * fix: update README to clarify provisioning environment as Poc instead of production-ready
    
    * fix: reorganize README to improve clarity on deployment steps and cleanup instructions
    
    * fix: update README link to provide more detailed instructions for deployment configurations

---
+++ cloudservice-setup/aws/eks/terraform/eks.tf
@@ -0,0 +1,37 @@
+module "eks-env" {
+  source = "../../../../kubernetes/aws/terraform"
+
+  region          = var.region
+  resource_suffix = var.resource_suffix
+
+  node_group = var.node_group
+}
+
+resource "aws_vpc_security_group_ingress_rule" "automq_console_ingress_rule" {
+  description                  = "Allow inbound traffic from security group of AutoMQ Console"
+  from_port                    = 0
+  to_port                      = 65535
+  ip_protocol                  = "tcp"
+  security_group_id            = module.eks-env.eks_cluster_security_group
+  referenced_security_group_id = module.automq-byoc.automq_byoc_security_group_id
+
+  depends_on = [module.automq-byoc, module.eks-env]
+}
+
+resource "aws_eks_access_entry" "cluster_admins" {
+  cluster_name      = module.eks-env.cluster_name
+  principal_arn     = module.automq-byoc.automq_byoc_console_role_arn
+  kubernetes_groups = []
+  type              = "STANDARD"
+  depends_on        = [module.automq-byoc, module.eks-env]
+}
+
+resource "aws_eks_access_policy_association" "cluster_admins" {
+  cluster_name  = module.eks-env.cluster_name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = module.automq-byoc.automq_byoc_console_role_arn
+  access_scope {
+    type = "cluster"
+  }
+  depends_on = [module.automq-byoc, module.eks-env]
+}
---
+++ cloudservice-setup/aws/eks/terraform/main.tf
@@ -0,0 +1,12 @@
+module "automq-byoc" {
+  source  = "AutoMQ/automq-byoc-environment/aws"
+  version = "0.3.1"
+
+  cloud_provider_region = var.region
+  automq_byoc_env_id    = var.resource_suffix
+
+  create_new_vpc                           = false
+  automq_byoc_vpc_id                       = module.eks-env.vpc_id
+  automq_byoc_env_console_public_subnet_id = module.eks-env.public_subnets[0]
+}
+
