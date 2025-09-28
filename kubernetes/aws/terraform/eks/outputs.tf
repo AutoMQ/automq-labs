b4d2589e02569f2077a62e9f2416092d4d3db6f9
Gezi-lzq <lzqtxwd@gmail.com>
2025-09-28 19:31:00 +0800

    feat: add outputs for EKS node group role name and ARN (#85)

--- kubernetes/aws/terraform/eks/outputs.tf
+++ kubernetes/aws/terraform/eks/outputs.tf
@@ -10,6 +10,14 @@ output "subnet_ids" {
   value = [for id in local.private_subnet_ids : id]
 }
 
+output "aws_eks_node_group_role_name" {
+  value = aws_iam_role.node_group_role.name
+}
+
+output "aws_eks_node_group_role_arn" {
+  value = aws_iam_role.node_group_role.arn
+}
+
 output "eks_cluster_security_group" {
   value = module.eks.cluster_primary_security_group_id
 }
