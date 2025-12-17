df42da258aeb6e8e56ce9741b6e32a08cb7a5360
lyx2000 <1419360299@qq.com>
2025-12-17 11:50:21 +0800

    Optimize the event producer logic

--- byoc-examples/features/table-topic/github-event/terraform/eks.tf
+++ byoc-examples/features/table-topic/github-event/terraform/eks.tf
@@ -1,5 +1,5 @@
 module "eks-env" {
-  source = "../../../setup/kubernetes/aws/terraform"
+  source = "../../../../setup/kubernetes/aws/terraform"
 
   region          = var.region
   resource_suffix = local.resource_suffix
